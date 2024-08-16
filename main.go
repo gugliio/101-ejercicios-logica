1aeef7a1fdf46e163fad9cc9eb69232a0411974d
Ariel Gugliotta <arielgugliotta89@gmail.com>
2024-08-16 00:01:06 -0300

    Add Reto 02 to main execution

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/gugliio/101-ejercicios-logica/anagrama"
 	"github.com/gugliio/101-ejercicios-logica/fizzbuzz"
 )
 
 func main() {
 	fmt.Println("Reto 01 - Fizzbuzz")
 	fizzbuzz.Execute()
+
+	fmt.Println("Reto 02 - Anagrama")
+	fmt.Println(anagrama.Execute("amor", "roma"))
 }
